a4f6e48a1c9bf223d74f803e784e8eddad131691
Rajesh-107 <applerb326@gmail.com>
2023-01-06 00:13:42 +0600

    visible toggle

---
+++ components/Form.js
@@ -0,0 +1,16 @@
+import React, { useReducer } from 'react';
+
+import AdduserForm from './AdduserForm';
+
+
+const Form = () => {
+
+
+    return (
+      <>
+        <AdduserForm></AdduserForm>
+      </>
+    );
+};
+
+export default Form;
